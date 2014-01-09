50f03f262ad2ebaa9f323e5997c52e6c84fb42ab
i3ick <Karlo@Aton>
2014-01-09 14:45:55 +0100

    commit

---
+++ Freezetag/src/me/i3ick/freezetag/PlayerSorter.java
@@ -0,0 +1,25 @@
+// THIS IS NOT A PLAYER SORTER. IT IS A PLAYER FREEZER EVENT!
+
+package me.i3ick.freezetag;
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerSorter implements Listener{
+	
+	private freezetagMain plugin;
+	
+	public PlayerSorter (freezetagMain plugin){
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerChat(AsyncPlayerChatEvent e){
+		if(plugin.frozen.contains(e.getPlayer().getName())){
+			e.getPlayer().teleport(e.getPlayer().getLocation());
+		}
+	}
+}
